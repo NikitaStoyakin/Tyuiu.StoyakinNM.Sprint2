269eff2c1f30c01c9d47ca230734632be1ed40ed
Neority <Tyuiu.StoyakinNM@outlook.com>
2024-10-06 10:59:06 +0300

    Add project files.

---
+++ Tyuiu.StoyakinNM.Sprint2.Task0.V21.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.StoyakinNM.Sprint2.Task0.V21.Lib
+{
+    public class DataService : ISprint2Task0V21
+    {
+        public bool[] GetCompareOperations(int x, int y)
+        {
+            bool[] res = new bool[6];
+
+            res[0] = x == y;
+            res[1] = x - 930 != y;
+            res[2] = x < y;
+            res[3] = x > y + 931;
+            res[4] = x <= y;
+            res[5] = x >= y + 931;
+
+            return res;
+        }
+    }
+}

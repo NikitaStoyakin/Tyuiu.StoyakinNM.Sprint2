397472507cfaf4ebd7d061d891ffdba9c5d7358f
Neority <Tyuiu.StoyakinNM@outlook.com>
2024-10-06 19:00:37 +0300

    Task 1

---
+++ Tyuiu.StoyakinNM.Sprint2.Task1.V6/Program.cs
@@ -0,0 +1,48 @@
+﻿
+using Tyuiu.StoyakinNM.Sprint2.Task1.V6.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        int a = 915;
+        int b = 169;
+        int c = 174;
+        int d = 133;
+        bool[] res = new bool[6];
+        res = ds.GetLogicOperations(a, b, c, d);
+        
+        Console.Title = "Спринт #2 | Выполнил: Стоякин Н.М. | ПКТб-24-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #2                                                               *");
+        Console.WriteLine("* Тема: Операции сравнения                                                *");
+        Console.WriteLine("* Задание #1                                                              *");
+        Console.WriteLine("* Вариант #6                                                              *");
+        Console.WriteLine("* Выполнил: Стоякин Никита Михайлович | ПКТб 24-1                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу из операций сравнений (==, !=, <, >, <=, >=,         *");
+        Console.WriteLine("* последовательность можно чередовать, но использовать один раз в         *");
+        Console.WriteLine("* выражении) и логических операций(|, &, ||, &&, !, ^, последовательность *");
+        Console.WriteLine("* операций не должна нарушаться), а также арифметических выражений,       *");
+        Console.WriteLine("* которая вернет логическую последовательность(массив): (False, False,    *");
+        Console.WriteLine("* True, False, True, False), при a =915, b = 169, c = 174, d = 133        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("a = " + a);
+        Console.WriteLine("b = " + b);
+        Console.WriteLine("c = " + c);
+        Console.WriteLine("d = " + d);
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ;                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        for (int i = 0; i < 6; i++)
+        {
+            Console.WriteLine(res[i]);
+        }
+    }
+}

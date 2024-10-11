da0385a4e6cb5956f2ed837a87b629ed8b55cb12
Neority <Tyuiu.StoyakinNM@outlook.com>
2024-10-11 14:42:06 +0300

    Task 3

---
+++ Tyuiu.StoyakinNM.Sprint2.Task3.V26.Tetst/DataServiceTest.cs
@@ -0,0 +1,47 @@
+using Tyuiu.StoyakinNM.Sprint2.Task3.V26.Lib;
+namespace Tyuiu.StoyakinNM.Sprint2.Task3.V26.Tetst
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCondition1()
+        {
+            DataService ds = new DataService();
+            double x = 1;
+            double res = ds.Calculate(x);
+            double wait = 3.851;
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void ValidCondition2()
+        {
+            DataService ds = new DataService();
+            double x = 0;
+            double res = ds.Calculate(x);
+            double wait = 15;
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void ValidCondition3()
+        {
+            DataService ds = new DataService();
+            double x = -1;
+            double res = ds.Calculate(x);
+            double wait = 0.171;
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void ValidCondition4()
+        {
+            DataService ds = new DataService();
+            double x = -31;
+            double res = ds.Calculate(x);
+            double wait = -216.968;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.StoyakinNM.Sprint2.Task3.V26/Program.cs
@@ -0,0 +1,35 @@
+﻿using Tyuiu.StoyakinNM.Sprint2.Task3.V26.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = "Спринт #2 | Выполнил: Стоякин Н.М. | ПКТб-24-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #2                                                               *");
+        Console.WriteLine("* Тема: Операции сравнения                                                *");
+        Console.WriteLine("* Задание #2                                                              *");
+        Console.WriteLine("* Вариант #22                                                             *");
+        Console.WriteLine("* Выполнил: Стоякин Никита Михайлович | ПКТб 24-1                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу на, которая запрашивает целые значения с клавиатуры  *");
+        Console.WriteLine("* и вычисляет находится ли точка с координатами X,Y в заштрихованной      *");
+        Console.WriteLine("* области.                                                                *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("Введите значение переменной X: ");
+        double x = Convert.ToDouble(Console.ReadLine());
+        double res = ds.Calculate(x);
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ;                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("Значение функции = " + res);
+
+        Console.ReadKey();
+    }
+}

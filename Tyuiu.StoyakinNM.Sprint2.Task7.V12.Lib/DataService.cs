4e9d9f26abf575fd3def541d6e2864666c21375b
Neority <Tyuiu.StoyakinNM@outlook.com>
2024-10-13 11:25:38 +0300

    Task 7

---
+++ Tyuiu.StoyakinNM.Sprint2.Task7.V12.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.StoyakinNM.Sprint2.Task7.V12.Lib
+{
+    public class DataService : ISprint2Task7V12
+    {
+        public bool CheckDotInShadedArea(double x, double y)
+        {
+            bool res;
+            if ((y > 0) && (y >= Math.Pow(x, 2)) && (x <= Math.Pow(y, 2)) || (y > 0) && (y <= 2 - x) && (x >= Math.Pow(x, 2)))
+            {
+                res = true;
+            }
+            else
+            {
+                res = false;
+            }
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.StoyakinNM.Sprint2.Task7.V12.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+using Tyuiu.StoyakinNM.Sprint2.Task7.V12.Lib;
+namespace Tyuiu.StoyakinNM.Sprint2.Task7.V12.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCheckDotInShadeArea()
+        {
+            DataService ds = new DataService();
+            double y = 1;
+            double x = 1;
+
+            bool res = ds.CheckDotInShadedArea(x, y);
+
+            bool wait = true;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
